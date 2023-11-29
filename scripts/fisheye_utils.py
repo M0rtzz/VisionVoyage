67d0b9e8abcc1099de00e7e89ac26bb666e8f85c
M0rtzz <m0rtzz@126.com>
2023-11-29 15:05:49 +0800

    add scripts dir

---
+++ scripts/fisheye_utils.py
@@ -0,0 +1,224 @@
+#!/home/m0rtzz/Program_Files/anaconda3/envs/py38/bin/python3
+
+import numpy as np
+import math
+import cv2
+import argparse
+
+
+def get_spherical_coordinates(FishSize):
+    """
+    基于输出图像的尺寸，确定输出图像上每个点的极坐标(r, phi)
+    Finds spherical coordinates on the output image
+    :param output_height: height of output image
+    :param output_width: width of output image
+    :return: two matrices that contain spherical coordinates
+    for all pixels of the output image
+    """
+    center = int(FishSize/2)
+    cc = (center, center)
+
+    y = np.arange(0, FishSize, 1)  # 返回一个有终点和起点的固定步长的排列（等差数组）
+    x = np.arange(0, FishSize, 1)
+
+    xx, yy = np.meshgrid(y, x)  # 将y、x中每个数据排列组合生成多个点，将各个点的x坐标放入xx中，y坐标放入yy中
+
+    bias = np.ones((FishSize, FishSize))*cc[0]  # 生成元素全为cc[0]的矩阵
+
+    xx = np.subtract(xx, bias)  # 将横坐标范围从0~output_height变为+-output_height/2
+    yy = np.subtract(yy, bias)
+    xx = np.divide(xx, bias)     # 横纵坐标标准化
+    xx[:, -1] = 1
+    yy = np.divide(yy, -bias)
+    yy[-1, :] = -1
+
+    r = np.sqrt(xx**2 + yy**2)  # 每个点的极坐标距离，并消除距离大于1的点
+    r[r > 1] = np.nan
+    r[r < 0] = 0
+
+    phi = np.zeros((FishSize, FishSize))   # 计算每个点的极坐标角度
+    phi[:center, center:] = np.arcsin(np.divide(yy[:center, center:], r[:center, center:]))
+    phi[:, :center] = np.pi - np.arcsin(np.divide(yy[:, :center], r[:, :center]))
+    phi[center+1:, center:] = 2*np.pi + np.arcsin(np.divide(yy[center+1:, center:], r[center+1:, center:]))
+    phi[cc[0], cc[1]] = 0
+
+    return r, phi
+
+
+def spherical_to_cartesian(r, phi, fov):
+    """
+    Transforms spherical coordinates to cartesian
+    :param r: matrix with computed pixel heights
+    :param phi: matrix with computed pixel angles
+    :param fov: desired field of view
+    :return: x,y,z cartesian coordinates
+    """
+    theta = r*fov/2
+    x = np.sin(theta)*np.cos(phi)
+    y = np.sin(theta)*np.sin(phi)
+    z = np.cos(theta)
+
+    return x, y, z
+
+
+def get_face(x, y, z):
+    """
+    根据三维笛卡尔坐标，确定该点位于立方体哪个表面上
+    Finds which face of a cube map a 3D vector with origin
+    at the center of the cube points to
+    :param x, y, z: cartesian coordinates
+    :return: string that indicates the face
+    """
+
+    max_axis = max(abs(x), abs(y), abs(z))
+
+    if math.isclose(max_axis, abs(x)):  # math.isclose 两个数是否绝对/相对接近
+        return 'right' if x < 0 else 'left'
+    elif math.isclose(max_axis, abs(y)):
+        return 'bottom' if y < 0 else 'top'
+    elif math.isclose(max_axis, abs(z)):
+        return 'back' if z < 0 else 'front'
+
+
+def raw_face_coordinates(face, x, y, z):
+    """
+    Finds u,v coordinates (image coordinates) for a given
+    3D vector
+    :param face: face where the vector points to
+    :param x, y, z: vector cartesian coordinates
+    :return: uv image coordinates
+    """
+    if face == 'left':
+        u = z
+        v = -y
+        ma = abs(x)
+    elif face == 'right':
+        u = -z
+        v = -y
+        ma = abs(x)
+    elif face == 'bottom':
+        u = -x
+        v = -z
+        ma = abs(y)
+    elif face == 'top':
+        u = -x
+        v = z
+        ma = abs(y)
+    elif face == 'back':
+        u = x
+        v = y
+        ma = abs(z)
+    elif face == 'front':
+        u = -x
+        v = -y
+        ma = abs(z)
+    else:
+        raise Exception('Tile ' + face + 'does not exist')
+
+    return (u/ma + 1)/2, (v/ma + 1)/2
+
+
+def normalized_coordinates(face, x, y, PicSize):
+    """
+    Finds coordinates on the 2D cube map image of a 3D
+    vector
+    :param face: face where a 3D vector points to
+    :param x, y: image coordinates
+    :param n: tiles size
+    :return: coordinates on the 2D cube map image
+    """
+    # 首先获取该表面的原点坐标
+    face_origin = {
+        'left': (0, PicSize),
+        'front': (PicSize, PicSize),
+        'right': (2*PicSize, PicSize),
+        'back': (3*PicSize, PicSize),
+        'top': (PicSize, 0),
+        'bottom': (PicSize, 2*PicSize),
+    }
+    tile_origin_coords = face_origin.get(face)
+
+    tile_x = math.floor(x*PicSize)    # math.floor 向下取整函数
+    tile_y = math.floor(y*PicSize)
+
+    if tile_x < 0:
+        tile_x = 0
+    elif tile_x >= PicSize:
+        tile_x = PicSize-1
+    if tile_y < 0:
+        tile_y = 0
+    elif tile_y >= PicSize:
+        tile_y = PicSize-1
+
+    x_cubemap = tile_origin_coords[0] + tile_x
+    y_cubemap = tile_origin_coords[1] + tile_y
+
+    return x_cubemap, y_cubemap
+
+
+def cube2fisheye(picture_group, PicSize, FishSize, FOV):
+    lPic, rPic, tPic, bPic, fPic = picture_group
+    cubemap = np.zeros((3*PicSize, 3*PicSize, 3))
+    cubemap[PicSize:2*PicSize, 0:PicSize] = lPic
+    cubemap[PicSize:2*PicSize, PicSize:2*PicSize] = fPic
+    cubemap[PicSize:2*PicSize, 2*PicSize:3*PicSize] = rPic
+    cubemap[0:PicSize, PicSize:2*PicSize] = tPic
+    cubemap[2*PicSize:3*PicSize, PicSize:2*PicSize] = bPic
+
+    fisheye_picture = np.zeros((FishSize, FishSize, 3))
+    fov = FOV*np.pi/180
+    r, phi = get_spherical_coordinates(FishSize)
+    x, y, z = spherical_to_cartesian(r, phi, fov)
+
+    for row in range(0, FishSize):
+        for column in range(0, FishSize):
+            if np.isnan(r[row, column]):    # 将输出图像平面上极坐标距离超过1的点设置为黑色
+                fisheye_picture[row, column, :] = 0
+            # 对于极坐标距离在1以内的点
+            else:
+                # 首先确定该点对应的三维坐标指向哪个立方体表面
+                face = get_face(x[row, column],
+                                y[row, column],
+                                z[row, column])
+                # 然后确定该点在该立方体表面上的uv坐标
+                u, v = raw_face_coordinates(face,
+                                            x[row, column],
+                                            y[row, column],
+                                            z[row, column])
+                # 最后获取标准化的uv坐标，锁定输出图像上坐标为(row,column)的点对应着立方体哪个表面上的哪个点
+                x_cubemap, y_cubemap = normalized_coordinates(face,
+                                                              u,
+                                                              v,
+                                                              PicSize)
+                # 将原图像素色彩值转移到输出图像上
+                fisheye_picture[row, column, :] = cubemap[y_cubemap, x_cubemap, :]
+
+    return fisheye_picture
+
+
+def main(input_images):
+
+    PicSize = 1024  # 立方体贴图每个面的尺寸
+    FishSize = 1344  # 鱼眼图像尺寸
+    FOV = 196  # 鱼眼视角
+    output_path = '/home/m0rtzz/Workspaces/VisionVoyage/test/test.png'
+    # 加载输入图像
+    lPic = cv2.imread(input_images[0])
+    rPic = cv2.imread(input_images[1])
+    tPic = cv2.imread(input_images[2])
+    bPic = cv2.imread(input_images[3])
+    fPic = cv2.imread(input_images[4])
+    # 生成鱼眼图像
+    fisheye_picture = cube2fisheye([lPic, rPic, tPic, bPic, fPic], PicSize, FishSize, FOV)
+
+    # 保存鱼眼图像
+    cv2.imwrite(output_path, fisheye_picture)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate fisheye image from cube map')
+    parser.add_argument('input_images', nargs=5, help='input cube map images')
+
+    args = parser.parse_args()
+    # output_path = '/home/m0rtzz/Workspaces/VisionVoyage/test'
+    main(args.input_images)
---
+++ scripts/gray2color.cc
@@ -0,0 +1,162 @@
+/**
+ * @file gray2color.cc
+ * @brief  ./gray2color.py c++重写
+ * @author M0rtzz E-mail : m0rtzz@163.com
+ * @version 1.0
+ * @date 2023-11-06
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <chrono>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+namespace fs = filesystem;
+
+/**
+ * @brief  显示剩余时间
+ */
+class ProgressBar
+{
+public:
+    explicit ProgressBar(int total, int width = 45) : total(total), width(width), start_time(std::chrono::high_resolution_clock::now()), progress(0) {}
+
+    void updateTime()
+    {
+        ++progress;
+        int remaining = total - progress;
+
+        float percentage = static_cast<float>(progress) / total;
+        int filled_width = static_cast<int>(width * percentage);
+
+        std::chrono::duration<double> elapsed_seconds = std::chrono::high_resolution_clock::now() - start_time;
+        double speed = elapsed_seconds.count() / progress;
+        std::chrono::duration<double> remaining_time = std::chrono::duration<double>(speed * remaining);
+
+        cout << "\r"
+             << "[" << string(filled_width, '#') << string(width - filled_width, ' ') << "] "
+             << int(percentage * 100.0) << "% "
+             << "Remaining Time: " << formatTime(remaining_time.count()) << flush;
+    }
+
+private:
+    int total;
+    int width;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+    int progress;
+
+    string formatTime(double seconds)
+    {
+        int hours = int(seconds / 3600);
+        int minutes = int((seconds - hours * 3600) / 60);
+        int secs = int(seconds - hours * 3600 - minutes * 60);
+
+        return to_string(hours) + "h " + to_string(minutes) + "m " + to_string(secs) + "s";
+    }
+};
+
+/**
+ * @brief  单通道转 RGB 通道
+ * @param  img_path
+ * @param  color_map
+ */
+void grayToColor(const string &img_path, const Mat &color_map)
+{
+    // 读取图片
+    Mat gray_img = imread(img_path, IMREAD_GRAYSCALE);
+    // 判断彩色图片是否已经存在
+    fs::path path(img_path);
+    string save_dir = "/home/m0rtzz/Program_Files/carla-0.9.14/PythonAPI/examples/carla-data-generator/data/fisheye/semantic_segmentation_CityScapesPalette";
+    if (!fs::exists(save_dir))
+    {
+        fs::create_directory(save_dir);
+    }
+    string save_path = (save_dir / path.filename()).replace_extension(".png");
+    if (fs::exists(save_path))
+    {
+        return;
+    }
+
+    // double min_val, max_val;
+    // Point min_loc, max_loc;
+    // minMaxLoc(gray_img, &min_val, &max_val, &min_loc, &max_loc);
+    // cout << " " << min_val << " " << max_val << '\n';
+
+    // 像素赋值
+    int h = gray_img.rows;
+    int w = gray_img.cols;
+    Mat color_img(h, w, CV_8UC3, Scalar(0, 0, 0));
+    for (int i = 0; i < color_map.rows; i++)
+    {
+        Mat index;
+        findNonZero(gray_img == i, index);
+        for (int j = 0; j < index.total(); j++)
+        {
+            Point pt = index.at<Point>(j);
+            color_img.at<Vec3b>(pt) = color_map.at<Vec3b>(i);
+        }
+    }
+    // 保存图片
+    cvtColor(color_img, color_img, COLOR_BGR2RGB);
+    imwrite(save_path, color_img);
+}
+
+int main()
+{
+    // 你的colormap
+    Mat cmap = (Mat_<Vec3b>(24, 1) << Vec3b(0, 0, 0), // unlabeled  0
+                Vec3b(128, 64, 128),                  // road  1
+                Vec3b(244, 35, 232),                  // sidewalk  2
+                Vec3b(70, 70, 70),                    // building  3
+                Vec3b(102, 102, 156),                 // wall  4
+                Vec3b(190, 153, 153),                 // fence  5
+                Vec3b(153, 153, 153),                 // pole  6
+                Vec3b(250, 170, 30),                  // traffic light  7
+                Vec3b(220, 220, 0),                   // traffic sign  8
+                Vec3b(107, 142, 35),                  // vegetation  9
+                Vec3b(152, 251, 152),                 // terrain  10
+                Vec3b(70, 130, 180),                  // sky  11
+                Vec3b(220, 20, 60),                   // pedestrian  12
+                Vec3b(255, 0, 0),                     // rider  13
+                Vec3b(0, 0, 142),                     // Car  14
+                Vec3b(0, 0, 70),                      // truck  15
+                Vec3b(0, 60, 100),                    // bus  16
+                // NOTE: 执行完 ./change_index 之后，需要加以修改
+                // Vec3b(0, 0, 0),                       // train
+                Vec3b(81, 0, 81),     // ground  17
+                Vec3b(0, 0, 230),     // motorcycle  18
+                Vec3b(119, 11, 32),   // bicycle  19
+                Vec3b(110, 190, 160), // static  20
+                Vec3b(170, 120, 50),  // dynamic  21
+                Vec3b(55, 90, 80),    // other  22
+                // Vec3b(45, 60, 150), // water  23
+                Vec3b(157, 234, 50) // road line  24
+                                    // Vec3b(81, 0, 81)      // ground  25
+    );
+    // 文件路径
+    // string img_dir = "/home/m0rtzz/Program_Files/carla-0.9.14/PythonAPI/examples/carla-data-generator/data/fisheye/semantic_segmentation_Raw";
+    string img_dir = "/home/m0rtzz/Program_Files/carla-0.9.14/PythonAPI/examples/carla-data-generator/data/fisheye/semantic_segmentation_Raw_changed_index";
+    // string img_dir = "/home/m0rtzz/Program_Files/carla-0.9.14/PythonAPI/examples/carla-data-generator/data/fisheye/1";
+    if (!fs::exists(img_dir))
+    {
+        fs::create_directory(img_dir);
+    }
+    int total_images = count_if(fs::directory_iterator(img_dir), fs::directory_iterator(), [](const fs::directory_entry &entry)
+                                { return entry.path().extension() == ".png"; });
+    ProgressBar progress(total_images);
+    for (const fs::directory_entry &entry : fs::directory_iterator(img_dir))
+    {
+        if (!entry.is_regular_file() || entry.path().extension() != ".png")
+        {
+            continue;
+        }
+        grayToColor(entry.path().string(), cmap);
+        progress.updateTime();
+    }
+    cout << endl;
+    return 0;
+}
