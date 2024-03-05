d44a0391097eb9e1240dc8f9e1eea1cab5c59b75
M0rtzz <m0rtzz@126.com>
2024-03-05 10:38:18 +0800

    prepare to release the packaged VisionVoyage Server

--- scripts/VisionVoyageServer.sh
+++ scripts/VisionVoyageServer.sh
@@ -2,13 +2,13 @@
 
 UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 # UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
-UE4_PROJECT_ROOT="/home/m0rtzz/Program_Files/CARLA_0.9.14-dirty"
+UE4_PROJECT_ROOT="/home/m0rtzz/Program_Files/VisionVoyage_Server"
 # gnome-terminal -- bash -c 'sleep 5s && wmctrl -r "CarlaUE4" -T "VisionVoyage Server"; exec bash'
 # gnome-terminal -- bash -c 'while true; do wmctrl -r "CarlaUE4" -T "VisionVoyage Server"; done'
 # gnome-terminal -- bash -c 'while true; do wmctrl -r "CarlaUE4" -T "VisionVoyage Server"; echo $?; done'
 # gnome-terminal -- bash -c 'while true; do wmctrl -r "CarlaUE4" -T "VisionVoyage Server"; code=$?; echo $code; if [ $code -eq 0 ]; then break; fi; done'
 # gnome-terminal -- bash -c 'while true; do wmctrl -r "CarlaUE4" -T "VisionVoyage Server"; code=$?; if [ $code -eq 0 ]; then break; fi; done; read -p "Press any key to exit..."'
 gnome-terminal -- bash -c 'while true; do wmctrl -r "CarlaUE4" -T "VisionVoyage Server"; code=$?; if [ $code -eq 0 ]; then break; fi; done'
-chmod +x "$UE4_PROJECT_ROOT/CarlaUE4/Binaries/Linux/VisionVoyage-UE4-Shipping"
-"$UE4_PROJECT_ROOT/CarlaUE4/Binaries/Linux/VisionVoyage-UE4-Shipping" CarlaUE4 "$@" 
+chmod +x "$UE4_PROJECT_ROOT/VisionVoyage-Server-UE4/Binaries/Linux/VisionVoyage-UE4-Shipping"
+"$UE4_PROJECT_ROOT/VisionVoyage-Server-UE4/Binaries/Linux/VisionVoyage-UE4-Shipping" CarlaUE4 "$@" 
 
