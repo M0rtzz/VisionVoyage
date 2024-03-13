01f31b7804b9c9942c69e318ac7320621b814fbb
M0rtzz <m0rtzz@126.com>
2024-03-13 18:13:48 +0800

    add comment in main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# 获取脚本所在路径
+
+# NOTE: fix BUG: ImportError: /lib/x86_64-linux-gnu/libgobject-2.0.so.0: undefined symbol: ffi_type_uint32, version LIBFFI_BASE_7.0
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libffi.so.7
+
+# 获取脚本所在路径
 run_path=$(dirname "$0")
 main_py_path="$run_path/main.py"
 cd "$run_path"
