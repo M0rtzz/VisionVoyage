63435426d7c8b819565e1b4177277c078cf09bec
M0rtzz <M0rtzz@noreply.gitcode.com>
2024-04-16 17:39:50 +0800

    🔥 remove(app_functions.py): remove some unused code

--- modules/app_functions.py
+++ modules/app_functions.py
@@ -36,10 +36,3 @@ class AppFunctions(MainWindow):
         # SET MANUAL STYLES
         self.ui.line_edit_filenames.setStyleSheet("background-color: #6272a4;")
         self.ui.btn_open_dir.setStyleSheet("background-color: #6272a4;")
-        # self.ui.plainTextEdit.setStyleSheet("background-color: #6272a4;")
-        # self.ui.tableWidget.setStyleSheet("QScrollBar:vertical { background: #6272a4; } QScrollBar:horizontal { background: #6272a4; }")
-        # self.ui.scrollArea.setStyleSheet("QScrollBar:vertical { background: #6272a4; } QScrollBar:horizontal { background: #6272a4; }")
-        # self.ui.comboBox.setStyleSheet("background-color: #6272a4;")
-        # self.ui.horizontalScrollBar.setStyleSheet("background-color: #6272a4;")
-        # self.ui.verticalScrollBar.setStyleSheet("background-color: #6272a4;")
-        # self.ui.commandLinkButton.setStyleSheet("color: #ff79c6;")
