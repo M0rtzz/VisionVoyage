8bef6533f989b885bad8b8ac09bef95bbd9d7ad6
M0rtzz <M0rtzz@noreply.gitcode.com>
2024-04-16 17:27:12 +0800

    🔨 script(main.sh): explicit variable name scope

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ export QT_LOGGING_RULES="qt.qpa.input.events=false"
 
 # 获取脚本所在路径
 run_path=$(dirname "$0")
-main_py_path="$run_path/main.py"
-cd "$run_path"
-"$main_py_path"
+main_py_path="${run_path}/main.py"
+cd "${run_path}"
+"${main_py_path}"
 exit
