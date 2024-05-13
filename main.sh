de1d3234303ac7ecc14078d4f3025efa68601e33
M0rtzz <M0rtzz@noreply.gitcode.com>
2024-05-13 21:47:45 +0800

    🧵 thread(main.sh): only allow single process access

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+LOCKFILE=/tmp/VisionVoyage.lock
+
+# 尝试获取锁，如果获取失败则退出
+exec 200>"${LOCKFILE}"
+flock -n 200 || {
+    echo "已经有一个实例在运行了！"
+    exit 1
+}
+
 # NOTE: fix BUG: ImportError: /lib/x86_64-linux-gnu/libgobject-2.0.so.0: undefined symbol: ffi_type_uint32, version LIBFFI_BASE_7.0
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libffi.so.7
 
@@ -9,6 +18,6 @@ export QT_LOGGING_RULES="qt.qpa.input.events=false"
 # 获取脚本所在路径
 run_path=$(dirname "$0")
 main_py_path="${run_path}/main.py"
-cd "${run_path}"
+cd "${run_path}" || exit
 "${main_py_path}"
 exit
