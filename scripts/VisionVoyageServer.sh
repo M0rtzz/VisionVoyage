06dabd5b0eb576158508ffcf58379cea8a6c0264
M0rtzz <m0rtzz@outlook.com>
2024-08-11 18:05:27 +0800

    ✨ feat: add code to get repo root dir

--- scripts/VisionVoyageServer.sh
+++ scripts/VisionVoyageServer.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
-UE4_PROJECT_ROOT="./server/VisionVoyage_Server"
-gnome-terminal -- bash -c 'while true; do wmctrl -r "CarlaUE4" -T "VisionVoyage Server"; code=$?; if [ $code -eq 0 ]; then break; fi; done'
+REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
+UE4_TRUE_SCRIPT_NAME=$(readlink -f "$0")
+UE4_PROJECT_ROOT="${REPO_ROOT_DIR}/server/VisionVoyage_Server"
+gnome-terminal -- bash -c "while true; do wmctrl -r 'CarlaUE4' -T 'VisionVoyage Server'; code=\$?; if [ \$code -eq 0 ]; then break; fi; done"
 chmod +x "${UE4_PROJECT_ROOT}/VisionVoyage-Server-UE4/Binaries/Linux/VisionVoyage-UE4-Shipping"
 "${UE4_PROJECT_ROOT}/VisionVoyage-Server-UE4/Binaries/Linux/VisionVoyage-UE4-Shipping" CarlaUE4 "$@"
