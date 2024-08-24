bbe00e607066e148dce9b3695c19d0d960cd57a1
M0rtzz <m0rtzz@outlook.com>
2024-08-24 20:03:33 +0800

    🚚 move: change VisionVoyageServer dir path

--- scripts/VisionVoyageServer.sh
+++ scripts/VisionVoyageServer.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
-UE4_TRUE_SCRIPT_NAME=$(readlink -f "$0")
-UE4_PROJECT_ROOT="${REPO_ROOT_DIR}/server/VisionVoyage_Server"
+UE4_PROJECT_ROOT="${REPO_ROOT_DIR}/server/VisionVoyageServer"
 gnome-terminal -- bash -c "while true; do wmctrl -r 'CarlaUE4' -T 'VisionVoyage Server'; code=\$?; if [ \$code -eq 0 ]; then break; fi; done"
-chmod +x "${UE4_PROJECT_ROOT}/VisionVoyage-Server-UE4/Binaries/Linux/VisionVoyage-UE4-Shipping"
-"${UE4_PROJECT_ROOT}/VisionVoyage-Server-UE4/Binaries/Linux/VisionVoyage-UE4-Shipping" CarlaUE4 "$@"
+chmod +x "${UE4_PROJECT_ROOT}/VisionVoyageServer-UE4/Binaries/Linux/VisionVoyageServer"
+"${UE4_PROJECT_ROOT}/VisionVoyageServer-UE4/Binaries/Linux/VisionVoyageServer" CarlaUE4 "$@"
